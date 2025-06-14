8389b12298149f7bd6566903b605c88382b577f2
Tim Brooks <tim@timbrooks.org>
2025-06-14 15:48:30 -0600

    WIP

--- server/src/main/java/org/elasticsearch/action/bulk/BulkShardRequest.java
+++ server/src/main/java/org/elasticsearch/action/bulk/BulkShardRequest.java
@@ -18,6 +18,8 @@ import org.elasticsearch.action.support.replication.ReplicatedWriteRequest;
 import org.elasticsearch.action.support.replication.ReplicationRequest;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.cluster.metadata.InferenceFieldMetadata;
+import org.elasticsearch.common.bytes.BytesArray;
+import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.util.set.Sets;
@@ -36,6 +38,7 @@ public final class BulkShardRequest extends ReplicatedWriteRequest<BulkShardRequ
     private static final long SHALLOW_SIZE = RamUsageEstimator.shallowSizeOfInstance(BulkShardRequest.class);
 
     private final BulkItemRequest[] items;
+    private final BytesReference compressedSources = BytesArray.EMPTY;
     private final boolean isSimulated;
 
     private transient Map<String, InferenceFieldMetadata> inferenceFieldMap = null;
